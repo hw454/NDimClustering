aceabba50d7206ab7b5fce976f859ef050721b22
Hayley Wragg <hayleywragg@hotmail.com>
2023-06-27 15:04:18 +0100

    Cluster based on axis columns. Comment out enrichment analysis.

--- Clustering.R
+++ Clustering.R
@@ -1,20 +1,9 @@
 ### 3) Clustering
 
-library(data.table)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-#library(xlsx)
-library(TwoSampleMR)
-
 # variables
 EXP_pheno = "21001"
 OUT_pheno = "845"
 pheno_irnt = TRUE  #maybe if true change all grepl to paste0(EXP_pheno,"_irnt")
-# Reset the working directory then change to data location
-res_dir=getwd()
-clus_dir = paste0(res_dir,"/working-example/data/")
-setwd(clus_dir)
 
 # cochran's Qtest >>>>
 q.meta.test = function(b_x, se_x){  #https://cjvanlissa.github.io/Doing-Meta-Analysis-in-R/heterogeneity-statistics.html
@@ -31,12 +20,15 @@ q.meta.test = function(b_x, se_x){  #https://cjvanlissa.github.io/Doing-Meta-Ana
 }
 
 #load QC-filtered data
-load(paste0(clus_dir,"/QCdata_",EXP_pheno,".Rdata"))
+load(paste0(res_dir,"QCdata_",EXP_pheno,".Rdata"))
 
 # k-means clustering data set up
-stdBeta_df_noEXP_nosus = stdBeta_df_noEXP[-sus_SNP_ind,]
+aim<-trait_axes[1]
+stdBeta_df_noEXP_nosus = stdBeta_df_noEXP[-sus_SNP_ind,aim]
 eff_df = abs(stdBeta_df_noEXP_nosus)
-eff_dfs = t(scale(t(eff_df)))
+if ( length(aim)<=1 ){
+  eff_dfs=scale(eff_df)
+  } else { eff_dfs = t(scale(t(eff_df)))}
 
 # Fitting K-Means clustering Model 
 kmeansIC = function(fit){
@@ -67,7 +59,7 @@ kmeans.minAIC = cluster_list[[which(IC_df$AIC==min(IC_df$AIC))]]
 nClust.AIC = max(kmeans.minAIC$cluster)
 
 # plot AIC
-pdf(file=paste0(res_dir,"/AIC-ncluster_",EXP_pheno,".pdf"), width=5, height = 7)
+pdf(file=paste0(res_dir,"AIC-ncluster_",EXP_pheno,".pdf"), width=5, height = 7)
 plot(IC_df$nCluster,IC_df$AIC, xlab="nCluster", ylab="AIC")
 abline(v=nClust.AIC,col="red")
 dev.off()
@@ -79,11 +71,11 @@ for(i in 1:nClust.AIC){
 }
 print(paste0("Number of SNPs in each AIC grouped clusters: ", paste0(lengths(AICclusters_rsid), collapse = ", ")))
 AICclusters_rsid_df = t(plyr::ldply(AICclusters_rsid, rbind))
-write.csv(AICclusters_rsid_df, paste0(res_dir,"/AICclusters_rsid_",EXP_pheno,".csv"), row.names = FALSE)
+write.csv(AICclusters_rsid_df, paste0(res_dir,"AICclusters_rsid_",EXP_pheno,".csv"), row.names = FALSE)
 
 # MR per cluster - AIC  - first instance of OUTCOME
-exp_dat = fread(paste0(clus_dir,"/sig-clumped-IVs_",EXP_pheno,".csv")) 
-out_dat = fread(paste0(clus_dir,"/clumped-IVs_",OUT_pheno,".csv")) 
+exp_dat = fread(paste0(data_dir,"sig-clumped-IVs_",EXP_pheno,".csv")) 
+out_dat = fread(paste0(data_dir,"clumped-IVs_",OUT_pheno,".csv")) 
 
 exp_dat2 = rename(exp_dat,"unstdBeta"="beta", "unstdSE"="se")
 out_dat2 = rename(out_dat,"unstdBeta"="beta", "unstdSE"="se")
@@ -119,7 +111,7 @@ if(all(exp_dat2$`effect_allele.exposure` == out_dat2$`effect_allele.outcome`)){
 '%!in%' <- function(x,y)!('%in%'(x,y))
 axy_ar = c()
 se_ar = c()
-MR_output = paste0(res_dir,"/",EXP_pheno,"-",OUT_pheno,"_AICclusters_MR.csv")
+MR_output = paste0(res_dir,EXP_pheno,"-",OUT_pheno,"_AICclusters_MR.csv")
 write.table("", file = MR_output, append = FALSE)
 for(clustN in 0:nClust.AIC){
   # harmonise the exposure and outcome data
@@ -168,11 +160,11 @@ cluster_axy$clusters = c("All", paste0("Cluster", 1:nClust.AIC))
 cluster_axy$clusters = factor(x = cluster_axy$clusters, levels = c(rev(paste0("Cluster", 1:nClust.AIC)),"All"))
 cluster_axy$boxsize = c(0.5,lengths(AICclusters_rsid)/sum(lengths(AICclusters_rsid)))
 
-write.csv(cluster_axy, paste0(res_dir,"/",EXP_pheno,"-",OUT_pheno,"_AICclusters_MRsummary.csv"), row.names = FALSE)
+write.csv(cluster_axy, paste0(res_dir,EXP_pheno,"-",OUT_pheno,"_AICclusters_MRsummary.csv"), row.names = FALSE)
 
 by_n <- function(n) { seq(-1000, 1000, by = n) } #https://stackoverflow.com/questions/52579553/scale-x-y-continuous-breaks-by-n-in-r-ggplot2
 
-pdf(file = paste0(clus_dir,"/",EXP_pheno,"-",OUT_pheno,"_AICclusters_MR.pdf"), width=8, height = 6)  ###getting corrupt
+pdf(file = paste0(res_dir,EXP_pheno,"-",OUT_pheno,"_AICclusters_MR.pdf"), width=8, height = 6)  ###getting corrupt
 p <- ggplot(cluster_axy, aes(y=clusters, x=axy_ar, xmin=axy_ar-(1.96*se_ar), xmax=axy_ar+(1.96*se_ar))) +
   geom_errorbarh(height=0,linewidth=0.5, alpha=0.3) +
   geom_point(size=cluster_axy$boxsize*10, shape = 15) + 
@@ -189,32 +181,36 @@ dev.off()
 q_test = q.meta.test(axy_ar,se_ar)
 print(paste0("Q-test of heterogeneity between cluster estimates = ", q_test[1],", p-value = ",q_test[2]))
 cat(paste0("Q-test of heterogeneity between cluster estimates = ", q_test[1],", p-value = ",q_test[2]),
-    file=paste0(res_dir,"/",EXP_pheno,"-",OUT_pheno,"_AICclusters_Qtest.txt"),sep="\n")
+    file=paste0(res_dir,EXP_pheno,"-",OUT_pheno,"_AICclusters_Qtest.txt"),sep="\n")
 
-## Enrichment analysis: getting traits specific to each cluster
-# perSNP heritability in each cluster, get the proportion of that for each trait (enrichment ratio)
-
-trait_h2 = matrix(NA, ncol = ncol(stdBeta_df_noEXP_nosus), nrow = nClust.AIC)
-for(i in 1:nClust.AIC){
-  c_df =  stdBeta_df_noEXP_nosus[which(rownames(stdBeta_df_noEXP_nosus) %in% AICclusters_rsid[[i]]),]^2
-  trait_h2[i,] = colSums(c_df)/nrow(c_df)
-}
-colnames(trait_h2) = colnames(stdBeta_df_noEXP_nosus)
-# normalise perSNP-h2 for each trait: dividing by the col mean
-trait_h2_norm = sweep(trait_h2,2,apply(trait_h2, 2,mean), "/")
-max_valind = apply(trait_h2_norm, 2, which.max)
-max_val = apply(trait_h2_norm, 2, max)
-traitPcluster = cbind.data.frame("trait" = colnames(trait_h2_norm), "cluster" = max_valind, "enrichmentRatio" = max_val)
-traitPcluster = inner_join(traitPcluster, trait_info_noEXP[,c(1,3)], by=c("trait"="phenotype"))
-
-traitPcluster_top10 = traitPcluster %>%
-  group_by(cluster) %>% slice_max(order_by = enrichmentRatio, n = 10)
-
-traitPcluster_top10 %>%
-  group_by(cluster) %>%
-  mutate(row = row_number()) %>%
-  tidyr::pivot_wider(names_from = cluster, values_from = c(trait, description, enrichmentRatio), names_vary = "slowest") %>%
-  select(-row) -> traitPcluster_top10
-
-write.csv(traitPcluster_top10, paste0(res_dir,"/",EXP_pheno,"-",OUT_pheno,"_TraitsTop10_AICclusters_perSNPh2.csv"), row.names = FALSE)
-write.csv(trait_h2_norm, paste0(res_dir,"/",EXP_pheno,"-",OUT_pheno,"_Traits_AICclusters_perSNPh2.csv"), row.names = FALSE)
+# ## Enrichment analysis: getting traits specific to each cluster
+# # perSNP heritability in each cluster, get the proportion of that for each trait (enrichment ratio)
+# 
+# #FIXME when column is one ncol function returns NULL as item is a vector rather than matrix
+# if (length(aim)<=1){ncols=1} else{ ncols=ncol(stdBeta_df_noEXP_nosus)}
+# trait_h2 = matrix(NA, ncol = ncols, nrow = nClust.AIC)
+# for(i in 1:nClust.AIC){
+#   row_ind=which(rownames(stdBeta_df_noEXP_nosus) %in% AICclusters_rsid[[i]])
+#   aim_ind=which(colnames(stdBeta_df_noEXP_nosus) %in% aim)
+#   c_df =  stdBeta_df_noEXP_nosus[row_ind,aim_ind]^2
+#   trait_h2[i,] = average(c_df)#colSums(c_df)/nrow(c_df)
+# }
+# colnames(trait_h2) = colnames(stdBeta_df_noEXP_nosus)
+# # normalise perSNP-h2 for each trait: dividing by the col mean
+# trait_h2_norm = sweep(trait_h2,2,apply(trait_h2, 2,mean), "/")
+# max_valind = apply(trait_h2_norm, 2, which.max)
+# max_val = apply(trait_h2_norm, 2, max)
+# traitPcluster = cbind.data.frame("trait" = colnames(trait_h2_norm), "cluster" = max_valind, "enrichmentRatio" = max_val)
+# traitPcluster = inner_join(traitPcluster, trait_info_noEXP[,c(1,3)], by=c("trait"="phenotype"))
+# 
+# traitPcluster_top10 = traitPcluster %>%
+#   group_by(cluster) %>% slice_max(order_by = enrichmentRatio, n = 10)
+# 
+# traitPcluster_top10 %>%
+#   group_by(cluster) %>%
+#   mutate(row = row_number()) %>%
+#   tidyr::pivot_wider(names_from = cluster, values_from = c(trait, description, enrichmentRatio), names_vary = "slowest") %>%
+#   select(-row) -> traitPcluster_top10
+# 
+# write.csv(traitPcluster_top10, paste0(res_dir,EXP_pheno,"-",OUT_pheno,"_TraitsTop10_AICclusters_perSNPh2.csv"), row.names = FALSE)
+# write.csv(trait_h2_norm, paste0(res_dir,EXP_pheno,"-",OUT_pheno,"_Traits_AICclusters_perSNPh2.csv"), row.names = FALSE)
