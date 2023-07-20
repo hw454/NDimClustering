ea48a9ac4c63a81b3c7f6e7e8695db370e435892
Hayley Wragg <hayleywragg@hotmail.com>
2023-07-20 10:20:17 +0100

    Store pval for association in a dataframe for each clustering stage. Iterate through trait list using trait labels rather than axis number. Take the absolute value of the association. - To be reviewed.

--- ClustScores.R
+++ ClustScores.R
@@ -1,7 +1,11 @@
-clust_score <- function(clusters_df,beta_df,aim_df){
-  Km <- length(unique(clusters_df$cluster))
-  SM <- length(clusters_df$clusters)
-  num_axis=length(aim_df$label)
+clust_score <- function(clusters_df,beta_df,pval_df,aim_df){
+  #' The dataframe of clusters is used alongside the beta and pvalue for each SNP
+  #' to score the clusters on their association with each trait.
+  #' Information for the traits is stored in aim_df
+  #Km <- length(unique(clusters_df$cluster))
+  #SM <- length(clusters_df$clusters)
+  traits <- aim_df$label
+  num_axis<-length(traits)
   clust_nums=unique(clusters_df$cluster)
   for (i in clust_nums) {
     # Store the cluster scores for each cluster number in c_score0
@@ -20,11 +24,12 @@ clust_score <- function(clusters_df,beta_df,aim_df){
     l1 <- length(SNP_list)
     c_score0 <- c_score0 %>% add_row(num_axis=num_axis,clust_num=i,
                                      clust_size=l1)
-    for (ai in 1:num_axis){
+    for (a in traits){
       # Add the cluster score for each axis to c_score0
-      a=aim_df$label[ai]
+      #a=aim_df$label[ai]
       # Track the number of terms in each cluster.
-      ax_score<-axis_score(beta_df,SNP_list,aim_df,a)
+      ax_score<-axis_score(beta_df,pval_df,SNP_list,aim_df,a)
+      # Account for the probability of being in the cluster
       c_score0['id']<-c_id
       if (!(a %in% colnames(c_score0))){
         c_score0[a]=ax_score
@@ -45,12 +50,12 @@ clust_score <- function(clusters_df,beta_df,aim_df){
   return(clust_scores)
 }
 
-axis_score <- function(beta_df,SNP_list,aim_df,a){
+axis_score <- function(beta_df,pval_df,SNP_list,aim_df,a){
   # Get the column index for the trait.
   b_cols <-aim_df[aim_df$label==a,'b_df_ind']
   # Get the row indices for the traits in the cluster
   b_rows<-which(rownames(beta_df) %in% SNP_list)
   # Get the pathway colocization score from the input data for the SNP
-  snp_assoc<-beta_df[b_rows,b_cols] # beta_df is a matrix with rows labelled by SNP_id and columns labelled by trait label.
+  snp_assoc<-abs(beta_df[b_rows,b_cols]*pval_df[b_rows,b_cols]) # beta_df is a matrix with rows labelled by SNP_id and columns labelled by trait label.
   return(mean(snp_assoc))
 }
