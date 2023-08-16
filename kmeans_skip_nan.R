95be71842fde4a7793f6984310064a62631fbb6d
Hayley Wragg <hayleywragg@hotmail.com>
2023-08-16 09:50:18 +0100

    Randomly assign initial SNP cluster. Reducing the bias of first cluster assignment through the loop ordering. Assign straight t dataframe rather than list and then creating dataframe later. Cluster probability assigned at end instead of in loops.

--- kmeans_skip_nan.R
+++ kmeans_skip_nan.R
@@ -1,66 +1,103 @@
-kmeans_skip_nan <- function(eff_dfs, centers = i, nstart = (nr+1), iter.max = 300,
-                            threshold=1e-5,norm_typ="F",na_rm=FALSE){
+kmeans_skip_nan <- function(eff_dfs, centers = nr, nstart = (nr+1), iter.max = 300,
+                            clust_threshold=1e-5,norm_typ="F",na_rm=TRUE){
   set.seed(123)
-  axes=names(eff_dfs)
-  centroids=data.frame(
+  centroids=data.frame(cluster=1:centers
   )
-  for(a in axes){
-    mina=min(eff_dfs[a],na.rm=na_rm)
-    maxa=max(eff_dfs[a],na.rm=na_rm)
-    centps=runif(i,mina,maxa)
-    centroids[a]=centps
+  axes_nonnan=c()
+  for(a in colnames(eff_dfs)){
+    mina=min(eff_dfs[,a],na.rm=na_rm)
+    maxa=max(eff_dfs[,a],na.rm=na_rm)
+    if (!is.na(mina) & !is.na(maxa)){
+      axes_nonnan<-axes_nonnan %>% append(a)
+      centps=runif(centers,mina,maxa)
+      centroids[a]=centps
+    }
   }
+  centroids<- centroids%>% column_to_rownames('cluster')
   max_dist=max(eff_dfs,na.rm=na_rm)-min(eff_dfs,na.rm=na_rm)
-  clusters=c()
-  clust_dist=c()
-  clust_prob=c()
+  # Randomly assign all SNPS a cluster number before iteration
+  cluster_df=data.frame(
+    snp_id=character(),
+    clusters=integer(),
+    clust_dist=numeric(),
+    clust_prob=numeric()
+  )
+  #FIXME
+  # Do the random assignment in dataframe setup not in loop
+  for(snp in rownames(eff_dfs)){
+    row=which(cluster_df$snp_id==snp)
+    snp_score=eff_dfs[snp,]
+    c_num=sample(1:centers,1)
+    cent=centroids[c_num,]
+    c_dist<-norm(data.matrix(na.omit(cent-snp_score)),norm_typ)
+    c_prob<-clust_prob_calc(c_dist)
+    cluster_df<-cluster_df %>% add_row(snp_id=snp,clusters=c_num,
+                                       clust_dist=c_dist,clust_prob=c_prob)
+  }
+  # Repeat for less cluster numbers and choose the min result
   for (iter in 1:iter.max){
-    for(snp_score in eff_dfs){
-      snp_dist=max_dist
-      snp_id=which(eff_dfs==snp_dist)
-      snp_clust_num=0
-      for(cent in centroids){
-        clust_num=which(centroids==cent)
-        dist=norm(na.omit(cent-snp_score),norm_typ)
+    #print('in iter')
+    #print(cluster_df)
+    # Initialise clusters for next iteration.
+    for(clust_snp_id in rownames(eff_dfs)){
+      snp_score=eff_dfs[clust_snp_id,]
+      snp_dist=cluster_df[cluster_df$snp_id==clust_snp_id,'clust_dist']
+      snp_clust_num=cluster_df[cluster_df$snp_id==clust_snp_id,'clusters']
+      for(clust_num in 1:centers){
+        cent=centroids[clust_num,]
+        dist=norm(data.matrix(na.omit(cent-snp_score)),norm_typ)
         if(dist<snp_dist){
           snp_dist=dist
           snp_clust_num=clust_num
         }
       }
-      clusters[snp_id]=snp_clust_num
-      clust_dist[snp_id]=snp_dist
-      clust_prob[snp_id]=1/(1+snp_dist)
+      row=which(cluster_df$snp_id==clust_snp_id)
+      cluster_df[row,'clust_dist']<-snp_dist
+      cluster_df[row,'clusters']<-snp_clust_num
+      cluster_df[row,'clust_prob']<-clust_prob_calc(snp_dist)
+      #print('After iter')
+      #print(cluster_df)
     }
+    snp_list<-rownames(eff_dfs)
     # Recompute the centroids based on the average of the clusters.
-    centroidscheck=data.frame()
+    centroidscheck<-centroids
     thresh_check=c()
     for (i in 1:length(centroids)){
-      snp_list=which(clusters==i)
-      snp_scores=eff_dfs[snp_list]
-      for(a in axes){
-        centroidscheck[i,a]=mean(snp_scores[a],na.rm=na_rm)
-      }
-      print(centroidscheck)
-      centroiddiff=na.omit(centroidscheck[i]-centroids[i])
-      centroidchange=norm(centroiddiff,norm_type)
-      if(centroidchange<threshold){
-        thresh_check<-append(thresh_check,TRUE)
-      }
-      else{
-        thresh_check<-append(thresh_check,FALSE)
+      snp_list=cluster_df[cluster_df$clusters==i,'snp_id']
+      snp_scores=eff_dfs[snp_list,]
+      if (length(snp_list)){
+        # Empty clusters not changed
+        for(a in axes_nonnan){
+          centroidscheck[i,a]=mean(eff_dfs[snp_list,a],na.rm=na_rm)
+        }
+        centroiddiff=data.matrix(na.omit(centroidscheck[i,axes_nonnan]-centroids[i,axes_nonnan]))
+        centroidchange=norm(centroiddiff,norm_typ)
+        if(centroidchange<clust_threshold & iter>1){
+          thresh_check<-append(thresh_check,TRUE)
+        }
+        else{
+          thresh_check<-append(thresh_check,FALSE)
+        }
       }
     }
     if(all(thresh_check)){
+      print('Clusters converged')
+      print(iter)
       break
+    }
+    else{
+      centroids=centroidscheck
     }
     # Are all the new centroids within the threshold of the previous
     
   }
- print('Maximum number of iterations reached without converging. ')
- cluster_df=data.frame(
-   clusters=clusters,
-   clust_dist=clust_dist,
-   clust_prob=clust_prob
- )
+ cluster_df<-column_to_rownames(cluster_df,'snp_id')
+ #print('end clust')
+ #print(cluster_df)
  return(cluster_df)
+}
+
+clust_prob_calc <-function(d){
+  dist=1.0/(1.0+d)
+  return(dist)
 }
