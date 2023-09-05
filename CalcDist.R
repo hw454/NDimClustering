edf54d69cbfca718e7b83c851df2df48b45e5822
Hayley Wragg <hayleywragg@hotmail.com>
2023-09-05 11:30:13 +0100

    Reduced loops with lapply.

--- CalcDist.R
+++ CalcDist.R
@@ -1,19 +1,33 @@
 setup_dist <- function(score_df,norm_typ="F"){
   # Setup distances
-  dist_df=data.frame(
-    snp1=character(),
-    snp2=character(),
-    dist=numeric()
+  dist_df = data.frame(
+    snp1 = character(),
+    snp2 = character(),
+    dist = numeric()
   )
-  for (snp1 in rownames(score_df)){
-    for (snp2 in rownames(score_df)){
-      x1=unstdBeta_df[rownames(unstdBeta_df)==snp1]
-      x2=unstdBeta_df[rownames(unstdBeta_df)==snp2]
-      xp=data.matrix(na.omit(x1-x2))
-      d=norm(xp,norm_typ)
-      dist_df<-dist_df %>% add_row(snp1=snp1,snp2=snp2,
-                           dist=d)
-    }
-  }
+  dist_list <- lapply(rownames(score_df), dist_col_calc,
+                      score_df = score_df,
+                      norm_typ = norm_typ)
+  dist_df <- Reduce(rbind, dist_list)
 return(dist_df)
+}
+
+dist_col_calc <- function(score_df, snp1, norm_typ = "F") {
+  dist_list <- lapply(rownames(score_df), pair_dist_calc,
+                      score_df = score_df, 
+                      snp1=snp1,
+                      norm_typ = norm_typ)
+  dist_df <- Reduce(rbind,dist_list)
+  return(dist_df)
+}
+
+pair_dist_calc <- function(score_df, snp1, snp2, norm_typ = "F") {
+  x1 <- score_df[rownames(score_df) == snp1]
+  x2 <- score_df[rownames(score_df) == snp2]
+  xp <- data.matrix(na.omit(x1-x2))
+  d <- norm(xp,norm_typ)
+  dist_df <- data.frame(snp1 = snp1,
+                      snp2 = snp2,
+                      dist=d)
+  return(dist_df)
 }
