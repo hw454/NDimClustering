429c3c954f45c37a9745c5314d3b8133f82af368
Hayley Wragg <hayleywragg@hotmail.com>
2023-09-13 12:04:09 +0100

    Inputs contained into lists of similar terms, Trait_df replaces aim_df as it represents the vairable information better. Output is df_list which contains the max difference dataframe, cluster scores, traits at each iteration and the transform for PCs at each iteration. Added norm_typ as input for the metric score.

--- ClusteringCompare.R
+++ ClusteringCompare.R
@@ -1,10 +1,16 @@
-clust_pca_compare <- function(unstd_beta_df, unstd_se_df, pval_df, # nolint
-                         diff_threshold, thresh_norm, clust_threshold,
-                         clus_norm, np = 3, nr = 10, which_clust = "basic",
-                         bp_on = TRUE, clust_prob_on = TRUE, narm = TRUE) {
+clust_pca_compare <- function(data_matrices, 
+                         thresholds = list("diff" = 1e-5, "clust" = 1e-5),
+                         na_handling = list("narm" = TRUE, "percent" = 0.95),
+                         iter_traits = list(iter = 0, 
+                                            "bp_on" = TRUE,
+                                            "clust_prob_on" = TRUE,
+                                            "clust_typ" = "basic"),
+                         norm_typs = list("clust" = "F", "thresh" = "F"),
+                         nums = list("max_dist" =1, "np" = 3, "nr" = 5)
+                        ) {
   #' Iterate through the columns in axes and clusters the data.
   #' If there is a distinct difference between two clusters exit.
-  aim_df <- data.frame(
+  trait_df <- data.frame(
     label = character(),
     axes_ind = integer(),
     b_df_ind = integer(),
@@ -23,7 +29,7 @@ clust_pca_compare <- function(unstd_beta_df, unstd_se_df, pval_df, # nolint
   c_scores <- add_np_pca_cols(c_scores, np)
   # Initialise with outcome
   out_col <- which(colnames(unstd_beta_df) == out_pheno)[1]
-  aim_df <- data.frame(label = OUT_pheno,
+  trait_df <- data.frame(label = out_pheno,
                        axes_ind = which(trait_info$phenotype == out_pheno)[1],
                        b_df_ind = out_col,
                        nSNPs = sum(!is.na(unstd_beta_df[, out_pheno]))[1]
@@ -32,194 +38,110 @@ clust_pca_compare <- function(unstd_beta_df, unstd_se_df, pval_df, # nolint
                        cn1 = integer(),
                        cn2 = integer(),
                        max_diff = integer())
-  for (ai in 1:length(trait_info$phenotype)){
+  df_list <- list("clust_scores" = c_scores,
+                  "max_diff" = max_df,
+                  "e_list" = list(),
+                  "trait" = list())
+  for (a in data_matrices$trait_info$phenotype){
     # Update the traits forming the axis
-    a <- trait_info$phenotype[ai]
     # Add the trait to the trait dataframe
-    covered <- (a %in% aim_df$label)
+    covered <- (a %in% trait_df$label)
     if (!covered) {
       # If the axis is already included then add row is not run.
-      aim_df <- aim_df_add_a(aim_df, a, trait_info$phenotype,
-                             unstd_beta_df)
-      allna <- all_na_check(unstd_beta_df)
-      if (allna) {
-        #' If the trait column was removed from the beta_df
-        #' during na removal then remove the trait from the
-        #' trait data frame and move to the next trait.
-        aim_df <- aim_df[aim_df$label != a, ]
-      } else {
+      allna <- na_col_check(data_matrices$b_df[, a], na_handling$percent)
+      if (!allna) {
+        # Add trait to trait dataframe
+        trait_df <- trait_df_add_a(trait_df, a, data_matrices$trait_info$phenotype)
+        trait_lab <- paste0("trait",num_axis)
+        trait_df <- append(df_list$trait, list(trait_lab = trait_df))
         # If the trait is not all NaN then run clustering.
-        print(paste0("PCA on ", length(aim_df$label), " axes"))
+        print(paste0("PCA on ", length(trait_df$label), " axes"))
         print(paste0("New axis ", a))
-              # Num axis
-        num_axis <- length(aim_df$label)
-        b_iter_df <- unstd_beta_df[, aim_df$label]
-        pval_iter_df <- pval_df[, aim_df$label]
-        # Cluster the data on these axes
+        # Num axis
+        num_axis <- length(trait_df$label)
         # Get the data upto this axis
-        b_df <- unstd_beta_df[,aim_df$label]
-        se_df <- unstd_se_df[,aim_df$label]
-        p_df <- pval_df[,aim_df$label]
-        pca_list   <- pca(b_df, p_df, se_df, np, narm)
+        b_iter_df <- data_matrices$beta[, trait_df$label]
+        se_iter_df <- data_matrices$se[, trait_df$label]
+        pval_iter_df <- data_matrices$pval[, trait_df$label]
+        # Cluster the data on these axes
+        pca_list   <- pca(b_iter_df, pval_iter_df, se_iter_df, nums$np, na_handling$narm)
         b_pc_mat    <- pca_list$beta
-        p_pc_mat    <- pca_list$pval
-        se_pc_mat   <- pca_list$se
+        p_pc_mat <- pca_list$pval
         t_mat       <- pca_list$transform
+        # Store the matrices for result output
+        #df_list$b_list <- append(df_list$b_list, list(a = b_pc_mat))
+        df_list$e_list <- append(df_list$e_list, list(a = t_mat))
         # Get column names for PCs
         pc_cols <- colnames(b_pc_mat)
         # Cluster the data on these axes
-        allna <- all_na_check(b_df)
-        # Cluster the data on these axes
-        if (which_clust == "min") {
-          cluster_df <- cluster_kmeans_min(b_pc_mat, nr)
+        if (iter_traits$clust_typ == "min") {
+          cluster_df <- cluster_kmeans_min(b_pc_mat,
+                                           nums$nr,
+                                           iter_traits$clust_prob_on,
+                                           norm_typs$clust,
+                                           thresholds$clusts, 
+                                           na_handling$narm)
         } else {
-          cluster_df <- cluster_kmeans_basic(b_pc_mat, nr,
-          clust_threshold, clust_norm)
-          }
+          cluster_df <- cluster_kmeans_basic(b_pc_mat,
+                                             nums$nr,
+                                             thresholds$clust, 
+                                             norm_typs$clust)
+        }
         # Find the set of cluster numbers
-        c_nums <- unique(cluster_df$cluster)
+        c_nums <- unique(cluster_df$clust_num)
         # Score the clustered data based on affiliations with axes.
         # Find the score for each PC
         c_score0 <- clust_score(cluster_df, beta_df = b_pc_mat, pval_df = p_pc_mat,
-                                bp_on = bp_on, clust_prob_on = clust_prob_on, 
+                                bp_on = iter_traits$bp_on, 
+                                clust_prob_on = iter_traits$clust_prob_on, 
                                 num_axis = num_axis)
         # Iterate through each cluster and compare across the others to find if
         # any pair have a distinct difference.
-        # Iterate through each cluster and compare across the others to find if
-        # any pair have a distinct difference.
-        n2 <- length(c_nums)
-        n2 <- n2 - 1
         diff_score_list <- lapply(c_nums, compare_oneclust_tolist,
-                                  c_nums = c_nums, c_score0 = c_score0,
-                                  axis = colnames(b_pc_mat))
-        diff_score_list <- diff_score_list[! sapply(diff_score_list, is.null)]
+                                  c_nums = c_nums, 
+                                  c_score0 = c_score0,
+                                  axis = pc_cols,
+                                  clust_norm = norm_typs$clust)
+        diff_score_list <- diff_score_list[ !sapply(diff_score_list, is.null)]
         diff_scores <- Reduce(rbind, diff_score_list)
+        # Find the pair with maximum cluster score difference and store in max_df0
         row <- which.max(diff_scores$diff)
         max_df0 <- diff_scores[row,]
-        max_df <-rbind(max_df0,max_df)
-        if (max(diff_scores$diff) > diff_threshold) {
-          print(paste("Threshold met on outcome", a))
-          return(c_scores)
-        }
+        max_df0["num_axis"] <- num_axis
+        df_list$max_diff <-rbind(df_list$max_diff, max_df0)
         c_score0["num_axis"] <- num_axis
-        c_scores <- rbind(c_scores, c_score0)
+        df_list$c_scores <- rbind(df_list$c_scores, c_score0)
+        if (max(diff_scores$diff) > thresholds$diff & iter> 10) {
+          print(paste("Threshold met on outcome", a))
+          return(df_list)
+        }
       }
     }
   }
   print("None of the outcomes clustering met the thresholding test. ")
-  out_list <- list("clust_scores" = c_scores,
-                   "max_diff" = max_df)
-  return(out_list)
+  return(df_list)
 }
 
-clust_compare <- function(unstd_beta_df, unstd_se_df, pval_df, tstat_df,
-                         threshold, thresh_norm, clust_threshold, nr,
-                         clust_norm, which_clust = "basic", bp_on = TRUE,
-                         clust_prob_on = TRUE) {
 
-  #' Iterate through the columns in axes and clusters the data.
-  #' If there is a distinct difference between two clusters exit.
-
-  aim_df <- data.frame(
-    label = character(),
-    axes_ind = integer(),
-    b_df_ind = integer(),
-    nSNPs = integer()
-  )
-  # Data frame for recording the cluster scores.
-  c_scores <- data.frame(
-    id = character(),
-    num_axis = integer(),
-    clust_num = integer(),
-    clust_size = integer(),
-    clust_prob = numeric(),
-    NA_Count = integer()
-  )
-  # Initialise with outcome
-  b_col <- which(colnames(unstd_beta_df) == OUT_pheno)[1]
-  aim_df <- data.frame(label = OUT_pheno,
-                       axes_ind = which(trait_info$phenotype == OUT_pheno)[1],
-                       b_df_ind = b_col,
-                       nSNPs = sum(!is.na(unstdBeta_df[, OUT_pheno]))[1]
-                       )
-  for (a in trait_info$phenotype) {
-    # Add the trait to the trait dataframe
-    covered <- (a %in% aim_df$label)
-    if (!covered) {
-      # If the axis is already included then add row is not run.
-      aim_df <- aim_df_add_a(aim_df, a, trait_info$phenotype,
-                 unstd_beta_df)
-      # Cluster the data on these axes
-      allna <- all_na_check(unstd_beta_df)
-      if (allna) {
-        #' If the trait column was removed from the beta_df
-        #' during na removal then remove the trait from the
-        #' trait data frame and move to the next trait.
-        aim_df <- aim_df[aim_df$label != a, ]
-      } else {
-        # If the trait is not all NaN then run clustering.
-        print(paste0("Cluster on ", length(aim_df$label), " axes"))
-        print(paste0("New axis ", a))
-        num_axis <- length(aim_df$label)
-        b_iter_df <- unstd_beta_df[, aim_df$label]
-        pval_iter_df <- pval_df[, aim_df$label]
-        # Cluster the data on these axes
-        if (which_clust == "min") {
-          cluster_df <- cluster_kmeans_min(b_iter_df, aim_df, nr)
-          } else {
-            cluster_df <- cluster_kmeans_basic(pval_iter_df, aim_df,
-            nr, clust_threshold, clust_norm)
-            }
-        # Find the set of cluster numbers
-        c_nums <- unique(cluster_df$cluster)
-        # Score the clustered data based on affilliations with axes.
-        # Find the score for this axis
-        c_score0 <- clust_score(cluster_df,
-                                b_iter_df, pval_iter_df,
-                                bp_on = bp_on, clust_prob_on = clust_prob_on,
-                                num_axis = num_axis) # Score across all axis
-        # Initialise new column for new axis
-        c_scores[a] <- numeric()
-        # Iterate through each cluster and compare across the others to find if
-        # any pair have a distinct difference.
-        n2 <- length(c_nums)
-        n2 <- n2 - 1
-        diff_score_list <- lapply(c_nums, compare_oneclust_tolist,
-                                  c_nums = c_nums, c_score0 = c_score0,
-                                  axis = aim_df$label)
-        diff_score_list <- diff_score_list[! sapply(diff_score_list, is.null)]
-        diff_scores <- Reduce(rbind, diff_score_list)
-        if (max(diff_scores$diff) > Threshold) {
-          print(paste("Threshold met on outcome", a))
-          return(c_scores)
-        }
-        c_score0["num_axis"] <- num_axis
-        c_scores <- rbind(c_scores, c_score0)
-      }
-    }
-  }
-  print("None of the outcomes clustering met the thresholding test. ")
-  return(c_scores)
-}
-
-compare_oneclust_tolist <- function(cn1, c_nums, c_score0, axis) {
+compare_oneclust_tolist <- function(cn1, c_nums, c_score0, axis, clust_norm = "F") {
   diff_score_list <- lapply(c_nums, clust_score_diff,
                             cn1 = cn1,
-                            cscore0 =  c_score0,
-                            axis = axis)
+                            c_score0 = c_score0,
+                            axis = axis,
+                            norm_typ = clust_norm)
   diff_score_list <- diff_score_list[!sapply(diff_score_list, is.null)]
   diff_scores <- Reduce(rbind, diff_score_list)
   return(diff_scores)
 }
 
-clust_score_diff <- function(cn1, cn2, c_score0, axis) {
+clust_score_diff <- function(cn1, cn2, c_score0, axis, norm_typ = "F") {
   #' Find the score difference between two clusters.
   #' If NaN return Null
   cs1 <- as.matrix(c_score0[c_score0$clust_num == cn1, axis])
   cs2 <- as.matrix(c_score0[c_score0$clust_num == cn2, axis])
   # If no members to cluster with score on that axis it will be NaN
   if (!all(is.na(cs1)) && !all(is.na(cs2)) && nrow(cs1) && nrow(cs2)) {
-    metric_score <- clust_metric(cs1, cs2, thresh_norm)
+    metric_score <- clust_metric(cs1, cs2, norm_typ)
     if (is.na(metric_score)) {
       return()
     } else {
