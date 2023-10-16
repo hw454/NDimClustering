3dc047c72475c201833838698da3348f2752864d
Hayley Wragg <hayleywragg@hotmail.com>
2023-10-16 17:44:56 +0100

    fix: Rename the distance calculation for a snp and the cluster it's a membership of.

--- ndimclustering/R/calc_member_dist_cent.R
+++ ndimclustering/R/calc_member_dist_cent.R
@@ -26,12 +26,11 @@
 #' @family clustering_components
 #'
 #' @export
-calc_dist_cent <- function(snp_id, b_mat, cluster_df, centroids_df,
+calc_member_dist_cent <- function(snp_id, b_mat, cluster_df, centroids_df,
                            norm_typ = "F") {
-   snp_score <- b_mat[snp_id, ]
    c_num <- cluster_df[snp_id, "clust_num"]
    cent <- centroids_df[c_num, ]
-   c_dist <- norm(data.matrix(stats::na.omit(cent - snp_score)), norm_typ)
+   c_dist <- calc_snp_cent_dist(snp_id, data_mat = b_mat, cent = cent)
    snp_clust_df <- data.frame(row.names = snp_id,
                        "clust_dist" = c_dist)
   return(snp_clust_df)
