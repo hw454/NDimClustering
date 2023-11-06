32d0c317bd88321dbb720cdeee1969a77730057d
Hayley Wragg <hayleywragg@hotmail.com>
2023-11-06 23:48:58 +0000

    fix: correct p to pc cols.

--- ndimclusteringR/R/make_pc_col.R
+++ ndimclusteringR/R/make_pc_col.R
@@ -3,13 +3,15 @@
 #' @details i is the input number
 #'
 #' @param i - The number for the column label
+#' @param rows - The rows for the dataframe,
+#'  defalut\: character() for an empty dataframe
 #'
 #' @return dataframe(Pi = numeric())
 #'
 #' @export
-make_p_col <- function(i, rows = character()) {
+make_pc_col <- function(i, rows = character()) {
   # Create a dataframe with column Pi
-  cname <- paste0("P", i)
+  cname <- paste0("PC", i)
   out <- data.frame(row.names = rows)
   if (length(rows) > 0) {
     out[cname] <- NA
