ca2f3e02a6b11ac1c863dd236b25f1190fc289bd
Hayley Wragg <hayleywragg@hotmail.com>
2023-11-06 23:55:08 +0000

    feat: Create function for regular scatter plots.

---
+++ ndimclusteringR/R/plot_scatter.R
@@ -0,0 +1,39 @@
+#' Plot the scatter plot or snps and principal components.
+#'
+#' @description
+#' Colour by cluster number.
+#' The x and y axis are the assocation with the principal component
+#' The width and height of the errorbars are given by the standard error.
+#'
+#' @param cluster_df The dataframe of cluster_df membership
+#' @param b_mat The matrix of the score data
+#' @param se_mat The matrix of the standarad errors associated with the scores.
+#' @param iter_traits The iteration variables for the type of iteration.
+#' @param num_axis The number of trait axis, default\:0
+#' @param pw The plot width, default\:8
+#' @param ph The plot heigh, default\:4
+#'
+#' @export
+plot_scatter <- function(b_mat,
+                          iter_traits,
+                          num_axis = 2,
+                          pw = 8,
+                          ph = 4) {
+  print(b_mat)
+  pnme <- paste0(iter_traits$res_dir, "scatter_num_axis_pc", num_axis, ".png")
+  c1 <- colnames(b_mat)[1]
+  c2 <- colnames(b_mat)[2]
+  snp_list <- row.names(b_mat)
+  res_df <- data.frame(
+    row.names = snp_list,
+    bx = b_mat[, c1],
+    by = b_mat[, c2]
+  )
+  ggplot2::ggplot(data = res_df,
+                  ggplot2::aes(x = bx, y = by)) + # nolint: object_usage_linter.
+  ggplot2::ylab("Association with PC2") +
+  ggplot2::xlab("Association with PC1") +
+  ggplot2::ggtitle("Clustered by principal components")
+  ggplot2::ggsave(filename = pnme, width = pw, height = ph)
+  return()
+}
