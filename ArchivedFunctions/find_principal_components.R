54bee7dcb7f34134f5fea4b5d8a2e450f560ca16
Hayley Wragg <es21851@bristol.ac.uk>
2023-11-17 17:46:47 +0000

    feat: include instructions for implementing function in archive

--- ArchivedFunctions/find_principal_components.R
+++ ArchivedFunctions/find_principal_components.R
@@ -1,36 +1,45 @@
-  #' Compute the np principal components of b_mat.
-  #'
-  #' @param b_mat - Data matrix, columns are axis, rows are points.
-  #' @param pval_mat - P-values associated with each point.
-  #' @param se_mat - Standard errors of the data points.
-  #' @param np - Integer, number of prinipal components, default \:3
-  #' @param narm - Bool to indicate whether remove NaN values in
-  #'   sample variables. default\: TRUE
-  #'
-  #' @description
-  #'   * Find the np largest Eigen values and eigen vectors.
-  #'   * Combine the vectors with column binding to form the transform
-  #'   matrix.
-  #'   * Transform the matrices into the princpal component space.
-  #'   * Combine the matrices into "out_list"
-  #'   \code{
-  #'   out_list   <- list("transform" = e_mat,
-  #'                  "beta" = b_pc_mat,
-  #'                   "pval" = pval_pc_mat,
-  #'                   "se" = se_pc_mat)
-  #'                   }
-  #'
-  #' @return out_list
-  #'
-  #' @export
+#' Compute the np principal components of b_mat.
+#'
+#' @param b_mat - Data matrix, columns are axis, rows are points.
+#' @param pval_mat - P-values associated with each point.
+#' @param se_mat - Standard errors of the data points.
+#' @param np - Integer, number of prinipal components, default \:3
+#' @param narm - Bool to indicate whether remove NaN values in
+#'   sample variables. default\: TRUE
+#'
+#' @description
+#'   * Find the np largest Eigen values and eigen vectors.
+#'   * Combine the vectors with column binding to form the transform
+#'   matrix.
+#'   * Transform the matrices into the princpal component space.
+#'   * Combine the matrices into "out_list"
+#'   \code{
+#'   out_list   <- list("transform" = e_mat,
+#'                  "beta" = b_pc_mat,
+#'                   "pval" = pval_pc_mat,
+#'                   "se" = se_pc_mat)
+#'                   }
+#'  To use this function:
+#'  pca_list <- find_principal_components(b_iter_mat,
+#'                                        pval_iter_mat,
+#'                                        se_iter_mat,
+#'                                        np,
+#'                                        narm)
+#'    b_pc_mat    <- pca_list$beta
+#'    p_pc_mat <- pca_list$pval
+#'    t_mat       <- pca_list$transform
+#'
+#' @return out_list
+#'
+#' @export
 find_principal_components <- function(b_mat, pval_mat, se_mat,
-                np = 3, narm = TRUE) {
+                                      np = 3, narm = TRUE) {
   # Normalise each Column
   std_b_mat <- calc_scale_mat(b_mat, narm)
   # Compute Correlation Matrix
   c_mat <- stats::cor(std_b_mat,
-                     method = "pearson",
-                     use = "pairwise.complete.obs")
+                      method = "pearson",
+                      use = "pairwise.complete.obs")
   # Find Eigen Vectors and Eigen Values
   e_mat <- find_np_eigen_mat(c_mat, np = np)
   # Ensure the transform rownames correspond to the axes in the original data.
