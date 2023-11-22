0dd4dac9904d175198506d5fee1b8c46770ed6d5
Hayley Wragg <es21851@bristol.ac.uk>
2023-11-22 19:15:32 +0000

    fix: Archived iterative maxdiff plot. Renamed function for standard to match filename.

--- ndimclusteringR/R/plot_mxdf.R
+++ ndimclusteringR/R/plot_mxdf.R
@@ -6,14 +6,18 @@
 #' @param ph the height of the plot. default \:4
 #'
 #' @export
-plot_max_diff <- function(max_df, iter_traits, pw = 16, ph = 4) {
+plot_mxdf <- function(max_df, iter_traits,
+  pw = 16, ph = 4
+) {
   method_str <- make_path_label_str(iter_traits)
   d_str <- make_full_desc_str(iter_traits)
   pnme <- paste0(iter_traits$res_dir,
-                 "NumAxis_Vs_MaxScoreDiff",
-                 method_str, ".png")
-  plot_title <- paste("Number of axis against max cluster difference score. 
-  \n Clustering type", d_str)
+    "NumAxis_Vs_MaxScoreDiff",
+    method_str, ".png"
+  )
+  plot_title <- paste("Number of axis against max cluster difference score.",
+    "\n Clustering type", d_str
+  )
   lineplot <- ggplot2::ggplot() +
     ggplot2::geom_line(data = max_df,
                        ggplot2::aes(x = "num_axis", y = "diff", group = 1),
@@ -22,7 +26,7 @@ plot_max_diff <- function(max_df, iter_traits, pw = 16, ph = 4) {
                         ggplot2::aes(x = "num_axis", y = "diff", group = 1),
                         color = "black") +
     ggplot2::ggtitle(plot_title)
-  lineplot
+  print(lineplot)
   ggplot2::ggsave(filename = pnme, width = pw, height = ph)
-  return()
+  return(lineplot)
 }
