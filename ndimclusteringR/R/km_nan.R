ce9ea1e5be5373a38d9f2507e22f418425329a49
Hayley Wragg <es21851@bristol.ac.uk>
2023-12-14 23:12:41 +0100

    feat: Set the column names of the centroids.

--- ndimclusteringR/R/km_nan.R
+++ ndimclusteringR/R/km_nan.R
@@ -6,14 +6,13 @@
 #'
 #' @param b_mat Matrix of the data, each row is a snp and the columns
 #'   are the traits.
+#' @param p_mat matrix of p-values corresponding to the scores in b_mat
 #' @param nclust The number of clusters to allocate. default\:5
 #' @param iter_max The maximum number of iterations to reach cluster
 #'   convergence. default\:300
 #' @param clust_threshold The threshold for how close cluster
 #'   centres need to be to be considered converged. default\:1e-5
-#' @param norm_typ The type of norm to be used in distance calculation.
-#'   The default is the Froebenius norm "F".
-#' @param prob_on Bool to indicate whether cluster probability is to be
+#' @param bin_p_clust Bool to indicate whether cluster probability is to be
 #'   assign. default\:TRUE
 #' @param how_cents How the centroids will be initialised. If "rand (default)"
 #'   then the coordinates are created using a uniform distribution on the range
@@ -40,25 +39,21 @@
 #' @return clust_out
 #'
 #' @export
-km_nan <- function(b_mat,
-                   nclust = 5,
-                   iter_max = 300,
-                   clust_threshold = 1e-5,
-                   norm_typ = "F",
-                   prob_on = TRUE,
-                   how_cents = "point") {
+km_nan <- function(b_mat, p_mat,
+  nclust = 5, iter_max = 300, clust_threshold = 1e-5,
+  bin_p_clust = TRUE, how_cents = "point"
+) {
   snp_list <- rownames(b_mat)
   # Generate data frame with max and min data.
   # Min and Max are columns. Rows for each column of b_mat
-  b_mat_no_na <- stats::na.omit(b_mat)
   if (grepl("rand", how_cents)) {
     min_max_df <- data.frame(row.names = colnames(b_mat),
-      min = apply(b_mat_no_na, 2, min),
-      max = apply(b_mat_no_na, 2, max)
+      min = apply(b_mat, 2, min),
+      max = apply(b_mat, 2, max)
     )
     min_max_df <- stats::na.omit(min_max_df)
     # Randomly assign centroids coords per cluster.
-    centroid_list <- lapply(rownames(min_max_df), make_rand_cent,
+    centroid_list <- lapply(colnames(b_mat), make_rand_cent,
                             n_cents = nclust, min_max_df = min_max_df)
     centroids_df <- Reduce(cbind, centroid_list)
   } else if (grepl("point", how_cents)) {
@@ -72,6 +67,7 @@ km_nan <- function(b_mat,
       centroids_df <- as.data.frame(b_mat[init_cent_id, ])
       rownames(centroids_df) <- seq_len(nclust)
     }
+    colnames(centroids_df) <- colnames(b_mat)
   }
   # Randomly assign cluster to snps
   nsnps <- length(snp_list)
@@ -84,17 +80,19 @@ km_nan <- function(b_mat,
   clust_dist_mem_list <- lapply(snp_list, calc_member_dist_cent,
                                 b_mat = b_mat,
                                 cluster_df = cluster_df,
-                                centroids_df = centroids_df,
-                                norm_typ = norm_typ)
+                                centroids_df = centroids_df)
   clust_dist_mem_df <- Reduce(rbind, clust_dist_mem_list)
-  cluster_df <- cbind(cluster_df, clust_dist_mem_df)
+  cluster_df <- cbind(rn = rownames(cluster_df),
+                      cluster_df,
+                      clust_dist_mem_df,
+                      row.names = NULL)
+  cluster_df <- tibble::column_to_rownames(cluster_df, var = "rn")
   for (iter in 1:iter_max){
     # For each SNP find the cluster with the closest centre.
     snp_clust_list <- lapply(snp_list, find_closest_clust_snp,
                              b_mat = b_mat,
                              cluster_df = cluster_df,
-                             centroids_df = centroids_df,
-                             norm_typ = norm_typ)
+                             centroids_df = centroids_df)
     # Combine the list of dataframes into one dataframe.
     # Override Cluster_df with the new assignment
     df_cols <- function(df, col) {
@@ -114,8 +112,9 @@ km_nan <- function(b_mat,
                           clustnum_df = cluster_df$clust_num,
                           b_mat = b_mat,
                           centroids_df = centroids_df,
-                          norm_typ = norm_typ,
-                          clust_threshold = clust_threshold)
+                          p_mat = p_mat,
+                          clust_threshold = clust_threshold,
+                          bin_p_clust = bin_p_clust)
     thresh_check_df <- Reduce(rbind, thresh_list)
     # Are all the new centroids within the threshold of the previous
     if (all(thresh_check_df$thresh_check)) {
@@ -126,9 +125,8 @@ km_nan <- function(b_mat,
                                     %in% c("thresh_check")]
     # Are all the new centroids within the threshold of the previous
   }
-  if (prob_on) {
-    cluster_df$clust_prob <- calc_clust_prob(cluster_df$clust_dist)
-  }
+  # Calculate the probability each snp is in the cluster
+  cluster_df$clust_prob <- calc_clust_prob(cluster_df)
   cluster_df <- dplyr::mutate(cluster_df, "ncents" = nclust)
   clust_out <- list("clusters" = cluster_df, "centres" = centroids_df,
                     "clust_dist" = clust_dist_df)
