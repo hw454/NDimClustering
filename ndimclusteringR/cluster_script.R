3ea1bae2c909b9d1dabb20162b99e3d2cef7e049
Hayley Wragg <es21851@bristol.ac.uk>
2023-12-14 23:10:32 +0100

    feat: Set the number of clusters to the number of pathways for a test case.

--- ndimclusteringR/cluster_script.R
+++ ndimclusteringR/cluster_script.R
@@ -3,18 +3,25 @@ library("ndimclusteringR")
 
 # Script for running the cluster program setting the program inputs
 data_dir <- "./TestData/paths"
+res_dir <- "./TestResults"
 for (i in 0:3){
+  if (i > 1) {
+    nc <- i
+  }
   iter_traits <- list(
     dname = paste0(data_dir, i, "/"),
     clust_type = "basic",
     pca_type = "prcomp",
-    n_clust = 3,
+    nclust = nc,
     n_pcs = 3,
     bin_angles = 1,
     bin_p_clust = 1,
     bin_p_score = 1,
     bin_d_score = 1,
-    nan_rm = 1
+    nan_rm = 1,
+    how_cents = "points"
   )
+  res_dir <- math_path_label_str(iter_traits)
+  iter_traits$res_dir <- res_dir
   clustering_program(iter_traits, test = 1)
 }
