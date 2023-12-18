6ca47f77b28b9b6ff4f04e75b170340a47f37108
Hayley Wragg <es21851@bristol.ac.uk>
2023-12-18 23:12:59 +0100

    fix: create directory if it doesn't exist. fix: Separate base directory and iteration directory.

--- cluster_script.R
+++ cluster_script.R
@@ -3,7 +3,11 @@ library("ndimclusteringR")
 
 # Script for running the cluster program setting the program inputs
 data_dir <- "./TestData/paths"
-res_dir <- "./TestResults/"
+res_dir0 <- "./TestResults/"
+if (!file.exists(res_dir0)){
+  dir.create(res_dir0)
+}
+res_dir0 <- paste0(res_dir0, "paths")
 for (i in 0:3){
   if (i > 1) {
     nc <- i
@@ -21,7 +25,11 @@ for (i in 0:3){
     nan_rm = 1,
     how_cents = "points"
   )
-  res_dir <- paste0(res_dir, make_path_label_str(iter_traits))
+  res_dir1 <- paste0(res_dir0, "paths", i)
+  if (!file.exists(res_dir1)){
+    dir.create(res_dir1)
+  }
+  res_dir <- paste0(res_dir1, "/", make_path_label_str(iter_traits))
   iter_traits$res_dir <- res_dir
   clustering_program(iter_traits, test = 0)
 }
