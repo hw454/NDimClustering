aa7eb3e280015402bc29f81065140c5e42df2beb
Hayley Wragg <es21851@bristol.ac.uk>
2024-01-08 18:25:26 +0000

    fix: Use subset to get rows with rownames as number.

--- ndimclusteringR/R/calc_member_dist_cent.R
+++ ndimclusteringR/R/calc_member_dist_cent.R
@@ -30,7 +30,9 @@ calc_member_dist_cent <- function(snp_id, b_mat, cluster_df, centroids_df,
   norm_typ = "F"
 ) {
   c_num <- cluster_df[snp_id, "clust_num"]
-  cent <- centroids_df[c_num, ]
+  cent <- subset(centroids_df,
+    rownames(centroids_df) == (c_num)
+  )
   snp_score <- b_mat[snp_id, ]
   c_dist <- norm(data.matrix(stats::na.omit(cent - snp_score)), norm_typ)
   snp_clust_df <- data.frame(row.names = snp_id,
